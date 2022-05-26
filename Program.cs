0d386042506404c84250ad2d52398b00b30031b8
coskunrumeysa <70705198+coskunrumeysa@users.noreply.github.com>
2022-05-26 15:37:24 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿//Beni Yaratan Elbet Yolumu Gösterir .Şuara-78
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int kk;
+            kk = 2;
+            int uk;
+            uk = 8;
+            int cevre;
+            cevre = 2*kk +2*uk;
+            Console.Write("Dikdörtgenin Çevresi :"+" "+cevre);
+            Console.Read();
+        }
+    }
+}
